c095f0a33a98af0ca4a884145113a249bcb98adc
dammug <50780040+dammug@users.noreply.github.com>
2022-02-14 23:20:27 -0600

    Agregar archivos modulo 10

---
+++ config.py
@@ -0,0 +1,19 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    
+    except FileNotFoundError:
+       print("Couldn't find the config.txt file!")
+
+    #except Exception:
+    #    print("Couldn't find the config.txt file!")
+
+    except PermissionError:
+        print("Found config.txt but couldn't read it")
+
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+
+if __name__ == '__main__':
+    main()
